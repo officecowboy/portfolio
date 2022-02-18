3933d80203288fbfafafcbd95c51b977ccfbd4cd
Scott Diekema <scott.diekema@gmail.com>
2022-02-18 16:58:58 -0500

    almost done

---
+++ rotate.js
@@ -0,0 +1,14 @@
+document.addEventListener("mousemove", function (event) {
+  const x = event.pageX
+  const y = event.pageY
+  
+  const midX = x - window.innerWidth / 2
+  const midY = y - window.innerHeight / 2
+  
+  const box = document.querySelector(".floppy")
+  
+  box.style.left = x + "px"
+  box.style.top = y + "px"
+  
+  box.style.transform = "rotateX(" + (midY * 0.5) + "deg) rotateY(" + (midX * 0.5) + "deg)"
+})
